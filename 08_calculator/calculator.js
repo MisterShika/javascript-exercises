b36d3deb8f8a796b947c62ca3ca1a3fef5ed7a30
Kyle W <125979751+MisterShika@users.noreply.github.com>
2024-01-30 08:07:21 +0900

    08 Exercise finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(inputArray) {
+	let theSum = 0;
+  inputArray.forEach(element => {
+    theSum += element;
+  });
+  return theSum;
 };
 
-const multiply = function() {
-
+const multiply = function(inputArray) {
+	let theMultiply = 1;
+  inputArray.forEach(element => {
+    theMultiply *= element;
+  });
+  return theMultiply;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let theFactorial = 1;
+  for(let i = x; i > 0; i--){
+    theFactorial *= i;
+  }
+  return theFactorial;
 };
 
 // Do not edit below this line
